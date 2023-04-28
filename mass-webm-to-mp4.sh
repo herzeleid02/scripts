188775ea7ec558e6bb181c2a67449c8f22c3cdb9
herzeleid02 <inxbox360.timosha@gmail.com>
2023-04-29 00:30:36 +0300

    work in progress

---
+++ mass-webm-to-mp4.sh
@@ -0,0 +1,19 @@
+# DO NOT USE IT
+
+#!/bin/bash
+
+if [ -z $1 ] || [ ! -d $1 ];
+	then
+	echo "Usage: [input dir] [output dir]"
+	exit 1
+fi
+
+input_dir=$1
+output_dir=$2
+
+echo $input_dir
+echo $output_dir
+
+
+for i in "$input_dir"/*.webm; do ffmpeg -i "$i" ""$output_dir"/${i%*}.mp4"; done
+exit 0
