9b0fb5820f2f7807708ae9a8035b53d2db07dc37
herzeleid02 <inxbox360.timosha@gmail.com>
2023-04-29 00:48:31 +0300

    added -n to ffmpeg so it wont overwrite existing mp4 files

--- mass-webm-to-mp4.sh
+++ mass-webm-to-mp4.sh
@@ -15,5 +15,5 @@ echo $input_dir
 echo $output_dir
 
 
-for i in "$input_dir"/*.webm; do ffmpeg -i "$i" ""$output_dir"/${i%*}.mp4"; done
+for i in "$input_dir"/*.webm; do ffmpeg -n -i "$i" ""$output_dir"/${i%*}.mp4"; done
 exit 0
