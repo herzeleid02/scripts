8e15e81e06be339aac07d44377c621db6a78ef3b
herzeleid02 <inxbox360.timosha@gmail.com>
2023-05-19 18:02:24 +0300

    hi

---
+++ aptscript.sh
@@ -0,0 +1,6 @@
+#!/bin/bash
+PACKAGES="snmp snmpd"
+apt-get download $(apt-cache depends --recurse --no-recommends --no-suggests \
+  --no-conflicts --no-breaks --no-replaces --no-enhances \
+  --no-pre-depends ${PACKAGES} | grep "^\w")
+
