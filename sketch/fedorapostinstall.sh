ce7c4ea2b6fce35e334790e6910c1d20ee2b1d66
herzeleid02 <inxbox360.timosha@gmail.com>
2023-09-18 20:43:14 +0300

    rewrote the script

--- sketch/fedorapostinstall.sh
+++ sketch/fedorapostinstall.sh
@@ -1,88 +1,106 @@
 #!/bin/bash
 gputype="" #var for gpu type (intel, amd, nvidia, other, optimus, intelamd)
-#gpuamount=$(cat ./fakelspci | grep -ice "VGA")  #debug
-gpuamount=$(lspci | grep -ice "VGA")
 
 function main() {
-	gpuprobeb
-	gpuprobea
+	gpuprobe
 }
 
-function distrocheck() {
+function check_distro() {
 echo ""
+}
+
+function check_privileges() {
+	if [[ "$EUID" -ne 0 ]]
+  		then echo "Please run as root"
+  	exit 1;
+	fi
+	
+}
+
+function flathub() {
+flatpak remote-add --if-not-exists flathub https://dl.flathub.org/repo/flathub.flatpakrepo
 }
 
 function rpmfusion() {
-	sudo dnf install https://mirrors.rpmfusion.org/free/fedora/rpmfusion-free-release-$(rpm -E %fedora).noarch.rpm https://mirrors.rpmfusion.org/nonfree/fedora/rpmfusion-nonfree-release-$(rpm -E %fedora).noarch.rpm -y
-	sudo dnf groupupdate core -y
-}
-
-function gpuprobec() {
-	# function for checking for amd+amd setups or intel+amd or intel+nvidia
-	# invoke only in case when u have 2 cards
-		
+	dnf upgrade --refresh -y
+	dnf install https://mirrors.rpmfusion.org/free/fedora/rpmfusion-free-release-$(rpm -E %fedora).noarch.rpm https://mirrors.rpmfusion.org/nonfree/fedora/rpmfusion-nonfree-release-$(rpm -E %fedora).noarch.rpm -y
+	dnf groupupdate core -y
 }
 
 
-
-function gpucount() {
-	#function for counting gpus
-	echo $gpuamount #debug
-	case $gpuamount in
-		"0")
-			echo "amogus -- no gpu" #debug
-			;;
-		"1")
-			echo "one gpu" # debug
-			#gpuprobea
-			;;
-		"2")
-			echo "two gpu" # debug
-			#gpuprobec
-			;;
-		*)
-			echo "AMOGUS" #debug
-			;;
-	esac
-
+function codecs_ffmpeg() {
+	#universal operation
+	dnf swap ffmpeg-free ffmpeg --allowerasing -y
+	dnf groupupdate multimedia --setop="install_weak_deps=False" --exclude=PackageKit-gstreamer-plugin -y
+	dnf groupupdate sound-and-video
 }
 
-function gpuprobea() {
-	#function for checking single gpu
-	gpustring=$(lspci | grep -i "VGA" | cut -c 36-38)
-	echo "$gpustring" #debug
+function gpuprobe() {
+	gpus=$(lspci -vmm | grep -A1  "VGA compatible controller" | grep -v "^Class:" | cut -d ':' -f 2-  | tr -d '\t' | grep -v "^--" | cut -d ' ' -f 1 | tr '[:upper:]' '[:lower:]' | sort -u | tr -d '\n')
+	echo "$gpus" #debug
 
-	case $(lspci | grep -i "VGA" | tr '[:upper:]' '[:lower:]' | cut -c 36-38) in
-		"nvi")
-			echo "Nvidia" #debug
-			gputype="nvidia"
+	case $gpus in 
+		"nvidia")
+			echo "nvidia" #debug
+			nvidiadrivers
 			;;
-		"adv")
-			echo "AMD Radeon" #debug
-			gputype="amd"
+		"intel")
+			echo "intel" #debug
+			codecs_intel
 			;;
-		"int")
-			echo "Intel" #debug
-			gputype="intel"
+		"advanced")
+			echo "amd" #debug
+			codecs_amd
+			;;
+		"intelnvidia")
+			echo "intelnvidia (optimus)" #debug
+			optimus
+			;;
+		"advancednvidia")
+			echo "advancednvidia (optimus)" #debug
+			optimus
+			;;
+		"advancedintel")
+			echo "amogus laptop" #debug
+			#codecs_intel
+			#codecs_amd
+			codecs_sneed
 			;;
 		"*")
-			echo "AMOGUS GPU" # debug
-			gputype="other"
-			;;
-	esac
-}
+			echo "sneed gpu" #debug
+		esac
 
-function gpubprobeb()
-	# function for dualgpu installs, dont invoke it when the gpu is 1
-	if 
 
-function codecs() {
-echo ""
 }
 
 function nvidiadrivers() {
-echo ""
+	dnf install akmod-nvidia xorg-x11-drv-nvidia-cuda nvidia-settings nvidia-vaapi-driver
+	# pulled from rpm fusion page
 }
+
+function codecs_intel(){
+	dnf install intel-media-driver
+}
+
+function codecs_amd(){
+	dnf swap mesa-va-drivers mesa-va-drivers-freeworld
+	dnf swap mesa-va-drivers.i686 mesa-va-drivers-freeworld.i686
+	dnf swap mesa-vdpau-drivers.i686 mesa-vdpau-drivers-freeworld.i686
+	dnf swap mesa-vdpau-drivers mesa-vdpau-drivers-freeworld	
+}
+
+function intel_optimus(){
+	echo "WIP"
+}
+
+function amd_optimus(){
+	echo "WIP"
+}
+
+function codecs_sneed(){
+	echo "sneed gpu"
+}
+
 
 function ascii() {
 	#cat << EOF
--- sketch/getgpu2.sh
+++ sketch/getgpu2.sh
@@ -1,5 +1,6 @@
 #!/bin/bash
 # neofetch 
 
-gpus=$(lspci -vmm | grep -A1  "VGA compatible controller" | grep -v "^Class:" | cut -d ':' -f 2-  | tr -d '\t' | cut -d ' ' -f 1 | tr '[:upper:]' '[:lower:]' | uniq)
+#gpus=$(lspci -vmm | grep -A1  "VGA compatible controller" | grep -v "^Class:" | cut -d ':' -f 2-  | tr -d '\t' | cut -d ' ' -f 1 | tr '[:upper:]' '[:lower:]' | uniq)
+gpus=$(cat ./fakelspci3 | grep -A1  "VGA compatible controller" | grep -v "^Class:" | cut -d ':' -f 2-  | tr -d '\t' | grep -v "^--" | cut -d ' ' -f 1 | tr '[:upper:]' '[:lower:]' | sort -u | tr -d '\n')
 echo "$gpus"
